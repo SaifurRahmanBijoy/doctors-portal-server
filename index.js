ccc5a2bd183586a11d0e743710317232de4ed067
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-21 22:32:19 +0600

    5th part

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
@@ -137,6 +138,19 @@ async function run() {
 
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1d",
+        });
+      }
+      // console.log(user);
+      res.status(403).send({ accessToken: "" });
     });
 
     app.post("/users", async (req, res) => {
